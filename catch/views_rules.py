96c0f9905e06e291916159c07a142cbb94f4ddd7
peteburn <peteburn@gmail.com>
2026-03-05 19:52:45 +0100

    Add rules to media

--- catch/views_rules.py
+++ catch/views_rules.py
@@ -7,7 +7,7 @@ import os
 class RulesView(View):
     def get(self, request):
         prev = request.META.get('HTTP_REFERER', reverse('home'))
-        rules_path = '/home/pete/Documents/multi-species-2026.pdf'
+        rules_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'media', 'multi-species-2026.pdf')
         if os.path.exists(rules_path):
             return FileResponse(open(rules_path, 'rb'), content_type='application/pdf')
         return render(request, 'rules_not_found.html', {'prev': prev})
